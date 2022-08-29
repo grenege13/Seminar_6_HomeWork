6f3e4cf4d260ec8ec6d0d373aabf3ac2c2d940ad
Ilya Fedorov <grenege13@gmail.com>
2022-08-29 23:55:10 +0300

    count number above null is done

---
+++ task_41/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.WriteLine("Введите количество чисел: ");
+int numberAmoumt = Convert.ToInt32(Console.ReadLine());
+
+int aboveNullCount = 0;
+
+for (int i = 0; i < numberAmoumt; i++)
+{
+    Console.WriteLine("Введите число: ");
+
+    int number = Convert.ToInt32(Console.ReadLine());   
+
+    if (number > 0) aboveNullCount++;
+}
+System.Console.WriteLine("Чисел больше 0 : " + aboveNullCount);
+
