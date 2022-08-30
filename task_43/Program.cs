df7c398ff142b3f8de51ea2fa7791ac053323643
Ilya Fedorov <grenege13@gmail.com>
2022-08-30 18:31:34 +0300

    task 43 started

--- task_43/Program.cs
+++ task_43/Program.cs
@@ -1,3 +1,15 @@
 ﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите значение b1 :");
+int b1Value = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение k1 :");
+int k1Value = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение b2 :");
+int b2Value = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите значение k2 :");
+int k2Value = Convert.ToInt32(Console.ReadLine());
