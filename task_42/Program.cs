7021d66ac935e25b88dbe5e2bd8d12c6c7d29a0d
Ilya Fedorov <grenege13@gmail.com>
2022-08-30 19:54:31 +0300

    task 42 wlole num convert to binary is done

--- task_42/Program.cs
+++ task_42/Program.cs
@@ -2,3 +2,31 @@
 // 45 -> 101101
 // 3 -> 11
 // 2 -> 10
+
+Console.WriteLine("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Бинарное число : ");
+// Console.Write(number / 2);
+
+string BinaryVar = "";
+
+while (number != 1)
+{
+    BinaryVar = BinaryVar + (number % 2);
+    
+    number = number / 2;
+
+    if (number == 1) 
+    {
+        BinaryVar = BinaryVar + 1;
+        break;
+    }
+}
+
+int BinaryVarLength = BinaryVar.Length;
+
+for (int i = BinaryVarLength - 1; i >= 0; i--)
+{
+    Console.Write(BinaryVar[i]);
+}
